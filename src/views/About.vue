60144e7bd16912652fcfcc0b59e542ee542ea769
Tons <tonsisouvanh7@gmail.com>
2022-06-05 17:42:02 +0700

    basic crud features added

---
+++ src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+  <h3>Basic CRUD app using VueJS</h3>
+  <router-link to="/">Go back</router-link>
+</template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
+      },
+    },
+  },
+});
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       '^/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         logLevel: 'debug',
+//         pathRewrite: { '^/api': '/' },
+//       },
+//     },
+//   },
+// }
